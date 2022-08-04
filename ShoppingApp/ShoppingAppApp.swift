e856559a615008630fec24625375e95c05780d89
ozan gulhan <ozangulhn@gmail.com>
2022-08-04 23:24:48 +0300

    Initial Commit

---
+++ ShoppingApp/ShoppingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingAppApp.swift
+//  ShoppingApp
+//
+//  Created by Ozan Gülhan on 4.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
